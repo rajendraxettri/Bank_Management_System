3b45a51f5903740cb70ca3f556f4abc01a9037df
rajendraxettri <xettrijr@gmail.com>
2023-10-03 08:34:27 +0545

    New REPO Updated  !

--- home.cpp
+++ home.cpp
@@ -1,10 +1,77 @@
 #include<iostream>
+#include<conio.h>
+#include<windows.h>
 using namespace std;
+class homeClass{
+public:
 
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+
+
+void welcome(){
+    cout<<"\t\t\t=============================================================================================================================== "<<endl;Sleep(70);
+    cout<<"\t\t\t=============   ==========    ==========     ==        ==       =========           ==           ==           ==    ==      =   "<<endl;Sleep(70);
+    cout<<"\t\t\t     ==         ==            ==             ==        ==       ==       ==        =  =          == =         ==    ==    =     "<<endl;Sleep(70);
+    cout<<"\t\t\t     ==         ==            ==             ==        ==       ==       ==       =    =         ==   =       ==    ==  =       "<<endl;Sleep(70);
+    cout<<"\t\t\t     ==         =======       ==             ============       =========        ========        ==     =     ==    ===         "<<endl;Sleep(70);
+    cout<<"\t\t\t     ==         ==            ==             ==        ==       ==       ==     =        =       ==       =   ==    ==  =       "<<endl;Sleep(70);
+    cout<<"\t\t\t     ==         ==            ==             ==        ==       ==       ==    =          =      ==         = ==    ==    =     "<<endl;Sleep(70);
+    cout<<"\t\t\t     ==         ==========    ==========     ==        ==       ==========    =            =     ==           ==    ==      =   "<<endl;Sleep(70);
+    cout<<"\t\t\t=============================================================================================================================== "<<endl;Sleep(70);
+    cout<<"\t\t\t\t\t\t\t\t\t               =         "<<endl;Sleep(70);   
+    cout<<"\t\t\t\t\t\t\t\t\t             =====       "<<endl;Sleep(70);    
+    cout<<"\t\t\t\t\t\t\t\t\t           =========     "<<endl;Sleep(70);   
+    cout<<"\t\t\t\t\t\t\t\t\t         =============   "<<endl;Sleep(70);    
+    cout<<"\t\t\t\t\t\t\t\t\t       ================= "<<endl;Sleep(70);      
+    cout<<"\t\t\t\t\t\t\t\t\t         =============   "<<endl;Sleep(70);    
+    cout<<"\t\t\t\t\t\t\t\t\t         ==  == ==  ==   "<<endl;Sleep(70);   
+    cout<<"\t\t\t\t\t\t\t\t\t         ==  == ==  ==   "<<endl;Sleep(70);   
+    cout<<"\t\t\t\t\t\t\t\t\t         ==  == ==  ==   "<<endl;Sleep(70);  
+    cout<<"\t\t\t\t\t\t\t\t\t       =================  "<<endl;Sleep(70);   
+    cout<<"\t\t\t\t\t\t\t\t\t       =================  "<<endl;Sleep(70);  
+    SetConsoleTextAttribute(h,4);
+	cout<<"\n\n\t\t\t\t\t\t\t\t\t";
+	cout<<"|";Sleep(60);
+	cout<<"<";Sleep(60);
+	cout<<"-";Sleep(60);
+	cout<<"- ";Sleep(60);
+	cout<<"W";Sleep(60);
+    cout<<"E";Sleep(60);
+    cout<<"L";Sleep(60);
+	cout<<"C";Sleep(60);
+	cout<<"O";Sleep(60);
+	cout<<"M";Sleep(60);
+	cout<<"E";Sleep(60);
+	cout<<" T";Sleep(60);
+	cout<<"O";Sleep(60);
+	cout<<" T";Sleep(60);
+	cout<<"E";Sleep(60);
+	cout<<"C";Sleep(60);
+	cout<<"H";Sleep(60);
+	cout<<" ";Sleep(60);
+	cout<<"B";Sleep(60);
+	cout<<"A";Sleep(60);
+	cout<<"N";Sleep(60);
+	cout<<"K";Sleep(60);
+	cout<<"!";Sleep(60);
+	cout<<" -";Sleep(60);
+	cout<<"-";Sleep(60);	
+	cout<<">";Sleep(60);
+	cout<<"|";Sleep(60);
+	getch();
+       
+    
+
+}
+
+};
 
 
 int main()
 {
+    homeClass home;
+    home.welcome();
+
 
 
 return 0;
